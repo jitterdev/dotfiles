09d31b9eb6d3555d70556e72e9385cc5bedf7cca
Jitter <jitterdev@users.noreply.github.com>
2025-12-19 14:04:27 -0600

    original colors and icons for new async git prompt

--- etc/fish/functions/fish_prompt.fish
+++ etc/fish/functions/fish_prompt.fish
@@ -11,7 +11,6 @@ end
 function _git_prompt_async --on-event fish_prompt
     command kill $_git_last_pid 2>/dev/null
 
-    # Check if in git repo
     set -l git_dir (command git rev-parse --git-dir 2>/dev/null)
     if test -z "$git_dir"
         set --universal $_git_info ""
@@ -19,38 +18,40 @@ function _git_prompt_async --on-event fish_prompt
     end
 
     fish --private --command "
-        set -l branch (
-            command git symbolic-ref --short HEAD 2>/dev/null ||
-            command git describe --tags --exact-match HEAD 2>/dev/null ||
-            command git rev-parse --short HEAD 2>/dev/null |
-                string replace --regex -- '(.+)' '@\$1'
-        )
+        set -l branch (command git symbolic-ref --short HEAD 2>/dev/null)
+        
+        if test -z \"\$branch\"
+            set branch '('(command git rev-parse --short HEAD 2>/dev/null)')'
+        end
 
-        set -l dirty
-        set -l untracked
-        set -l stash
+        set -l staged (command git diff --cached --numstat 2>/dev/null | count)
+        set -l modified (command git diff --numstat 2>/dev/null | count)
+        set -l untracked (command git ls-files --others --exclude-standard 2>/dev/null | count)
+        set -l stash (command git stash list 2>/dev/null | count)
 
-        command git diff --quiet HEAD 2>/dev/null || set dirty '!'
-        test -n \"\$(command git ls-files --others --exclude-standard 2>/dev/null)\" && set untracked '?'
-        command git rev-parse --verify refs/stash &>/dev/null && set stash '\$'
+        set -l flags ''
+        test \$staged -gt 0 && set flags \"\$flags●\$staged\"
+        test \$modified -gt 0 && set flags \"\$flags✚\$modified\"
+        test \$untracked -gt 0 && set flags \"\$flags…\$untracked\"
+        test \$stash -gt 0 && set flags \"\$flags⚑\$stash\"
 
-        set -l upstream
+        set -l upstream ''
         command git rev-list --count --left-right @{upstream}...@ 2>/dev/null | read -l behind ahead
         
         if test -n \"\$ahead\" -a -n \"\$behind\"
             if test \"\$ahead\" != 0 -a \"\$behind\" != 0
-                set upstream \" ↑\$ahead ↓\$behind\"
+                set upstream \"↑\$ahead↓\$behind\"
             else if test \"\$ahead\" != 0
-                set upstream \" ↑\$ahead\"
+                set upstream \"↑\$ahead\"
             else if test \"\$behind\" != 0
-                set upstream \" ↓\$behind\"
+                set upstream \"↓\$behind\"
             end
         end
 
-        set -l flags \"\$dirty\"\"\$untracked\"\"\$stash\"
-        test -n \"\$flags\" && set flags \" \$flags\"
+        test -n \"\$flags\" && set flags \"|\$flags\"
+        test -n \"\$upstream\" && set flags \"\$flags\$upstream\"
 
-        set --universal $_git_info \"\$branch\$flags\$upstream\"
+        set --universal $_git_info \"\$branch\$flags\"
     " &
 
     set --global _git_last_pid $last_pid
@@ -70,8 +71,9 @@ function fish_prompt
     set -l color_user (set_color blue)
     set -l color_host (set_color b4befe)
     set -l color_dir (set_color f5c2e7)
-    set -l color_git (set_color 89dceb)
-    set -l color_flags (set_color f38ba8)
+    set -l color_git_bracket (set_color 89dceb)
+    set -l color_branch (set_color normal)
+    set -l color_flags (set_color brred)
     set -l normal (set_color normal)
 
     set -l symbol '$'
@@ -83,9 +85,16 @@ function fish_prompt
     # Git info
     set -l info $$_git_info
     if test -n "$info"
-        set -l branch (string split ' ' -- $info)[1]
-        set -l rest (string replace -- "$branch" '' "$info")
-        echo -n $separator'-'$brackets'['$color_git$branch$color_flags$rest$brackets']'
+        set -l parts (string split '|' -- $info)
+        set -l branch $parts[1]
+        set -l flags ""
+        test (count $parts) -gt 1 && set flags $parts[2]
+        
+        echo -n $color_git_bracket'-['$color_branch$branch
+        if test -n "$flags"
+            echo -n $normal'|'$color_flags$flags
+        end
+        echo -n $color_git_bracket']-'
     end
 
     echo
